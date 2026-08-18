6fdc419b78e141050476b221f9d5de0e14e8ef7f
Limbuarin <limbuaneel11@gmail.com>
2026-08-18 23:46:53 +0545

    completed the first basic  react porject

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,8 +1,16 @@
 const Navbar = () => {
     return (
-        <h1>Navbar goes here</h1>
+        <header>
+            <nav>
+                <img src="./react-logo.png" alt="react logo"/>
+                <span>ReactFacts</span>
+            </nav>
+        </header>
     )
 
 } 
 
+// background-color: ;
+// color: #61DAFB
+// font-size: 1.4rem;  
 export default Navbar;
