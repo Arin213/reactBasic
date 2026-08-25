23300109c86172970e50c8b8083566f49e9326e8
Limbuarin <limbuaneel11@gmail.com>
2026-08-25 21:49:57 +0545

    stated with part2 of first & second session of journal entry

---
+++ part2-dataDrivenReact/src/components/Header.jsx
@@ -0,0 +1,9 @@
+export default function Header() {
+    return(
+        <header>
+            <img src="/Globe.png" alt="globe-image" />
+            <h1>my travel journal.</h1>
+        </header>
+    ) 
+
+}
